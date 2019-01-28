3017c0ab9ae5f12f021ef4346aa03220116bb6ef
Quallenauge <Hamsi2k@freenet.de>
2019-01-28 19:40:20 +0100

    camera: Enable API override for mm-qcamera-daemon.
    mm-qcamera-daemon uses libraries which tries to release
    their mutexes also in case they are already released.
    This behavior is forbidden in current android versions.
    
    Lineage defines some per-binary exceptions to define a
    lower SDK version for only a set of explicitely specified binaries.
    
    Change-Id: I6757aed9ecc7455b66e779cb9116217846ac2029
    Signed-off-by: Albert I <kras@raphielgang.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,6 +95,11 @@ BOARD_QTI_CAMERA_32BIT_ONLY := true
 TARGET_TS_MAKEUP := true
 USE_DEVICE_SPECIFIC_CAMERA := true
 
+# API Override
+TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
+    /vendor/bin/mm-qcamera-daemon=27 \
+    /system/vendor/bin/mm-qcamera-daemon=27 \
+
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
